c5678f15c697a08bc96cd077cd2ea007aa7b513a
Hunter Lacefield <lace1010@gmail.com>
2021-02-23 14:48:31 +0800

    Added mongoose and mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const mongoose = require("mongoose"); // Need to require mongoose
+//** 1) Install and set up mongoose. (connected it to heroku as well.)
+mongoose.connect(process.env.MONGO_URI, {
+  // The MONGO_URI string is in sample.env. Be sure to change <password> to the user's actual password for mongoose to connect to the database
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -27,19 +35,7 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.post("/api/shorturl/new", (req, res) => {
-  console.log(
-    req.body.url.slice(0, 12),
-    "<= first part of string to match https://www."
-  );
-
-  //https://www.
-  console.log(req.body.url.slice(req.body.url.length - 4));
-
-  let lastFourURL = req.body.url.slice(req.body.url.length - 4);
-  if (
-    (req.body.url.slice(0, 12) == "https://www." && lastFourURL == ".com") ||
-    lastFourURL == ".org"
-  ) {
+  if (req.body.url) {
     res.json({ original_url: req.body.url, short_url: "number" });
   } else {
     res.json({ error: "invalid url" });

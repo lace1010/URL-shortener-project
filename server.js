7401e4856ff94fc2556e926a03a5b4fab86a88e0
Hunter Lacefield <lace1010@gmail.com>
2021-02-23 12:38:35 +0800

    Seeing if the logic for valid url is working

--- server.js
+++ server.js
@@ -6,19 +6,46 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// app.Post does not work unless we use body-parser middleware function here and call on the following two app.use
+const bodyParser = require("body-parser"); // Must add bodyParser middleware to get info from body in html for .post()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // In order to parse JSON data sent in the POST request,
+
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function () {
+app.post("/api/shorturl/new", (req, res) => {
+  console.log(
+    req.body.url.slice(0, 12),
+    "<= first part of string to match https://www."
+  );
+
+  //https://www.
+  console.log(req.body.url.slice(req.body.url.length - 4));
+
+  let lastFourURL = req.body.url.slice(req.body.url.length - 4);
+  if (
+    (req.body.url.slice(0, 12) == "https://www." && lastFourURL == ".com") ||
+    lastFourURL == ".org"
+  ) {
+    res.json({ original_url: req.body.url, short_url: "number" });
+  } else {
+    res.json({ error: "Invalid URL" });
+  }
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
